cd3d19a8fa612c70710d260f760d5d653cc58898
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2022-09-09 10:50:21 +0200

    Nedtellings-eksempel ferdig

--- jobjornar/html og css/nedtelling/kode.js
+++ jobjornar/html og css/nedtelling/kode.js
@@ -1,21 +1,43 @@
-setInterval(tikk, 1000);
+let nedtelling = setInterval(tikk, 1000);
 
-let antallSekunder = prompt("Hvor mange sekunder?");
+let antallSekunder = prompt("Hvor mange sekunder?"); // Spør brukaren kor mange sekunder og lagrar det i variabelen antallSekunder
+
+let musikkFerdig = new Audio("lyd/happy-day-113985.mp3");
+let musikkBakgrunn = new Audio("lyd/just-a-click-away-sci-fi-background-music-109864.mp3");
 
 function tikk() {
     antallSekunder = antallSekunder - 1; // Trekk fra en på antall sekunder
     console.log(antallSekunder); // Skriv ut antall gjenværende sekund
     document.getElementById("utskrift").innerText = antallSekunder;
 
+    if (antallSekunder <= 3) {
+        document.getElementById("utskrift").style.color = "red"; // Skriftfarge for h1 til rød
+    }
+
     if (antallSekunder <= 0) {
-        document.getElementById("utskrift").innerText = "Gratulerer!";
+        document.getElementById("utskrift").innerText = "Gratulerer, du kan gå!";
+        document.getElementById("utskrift").style.color = "green"; // Skriftfarge for h1 til grønn
+        musikkFerdig.play(); // Startar musikken som speler når ferdig nedtelling
+        musikkBakgrunn.pause(); // Stoppar bakgrunnsmusikken
+        clearInterval(nedtelling); // Stoppar den automatiske køyringa av "tikk"
     }
 }
 
+document.body.addEventListener("click", spillMusikk);
+
+function spillMusikk() {
+    musikkBakgrunn.play();
+}
+
+// Alternativ løysing til det over
+// document.body.addEventListener("click", function() {
+//      ...
+// });
+
 /*
     To-do:
-    - endre stilark etter behov: eks. når 3 sek. igjen, rød skriftfarge,
+    x endre stilark etter behov: eks. når 3 sek. igjen, rød skriftfarge,
       når ferdig, grønn skrift.
-    - bakgrunnsmusikk (NB: autoavspeling av lyd er fy-fy)
-    - musikk når ferdig
+    x bakgrunnsmusikk (NB: autoavspeling av lyd er fy-fy)
+    x musikk når ferdig
 */
