28f1b8974f2589527c29d59946a2f5c87e610bbc
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2022-10-04 12:31:41 +0200

    Forsøk: endring av timer live

--- temp/tilfeldigFarge/v1-automatisk/kode.js
+++ temp/tilfeldigFarge/v1-automatisk/kode.js
@@ -34,6 +34,54 @@ function getRandomIntInclusive(min, max) {
     Delen av koden som får fargeendringane til å skje ved faste intervall.
     ----------------------------------------------------------------------
 */
-const fargeEndring = setInterval(function() {
+let tidsintervall = 1000;
+
+let fargeEndring = setInterval(function() {
     endreBakgrunnsfargeRGB();
-}, 500);
+}, tidsintervall);
+
+/*
+    -----
+    Modal
+    -----
+*/
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const innstillingar = document.getElementById("innstillingar");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+innstillingar.addEventListener("click", function() {
+  modal.style.display = "block";
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
+
+/*
+    --------
+
+    --------
+*/
+// const inpEndringIntervall = document.getElementById("inpEndringIntervall");
+// inpEndringIntervall.addEventListener("change", function() {
+//     tidsintervall = parseInt(inpEndringIntervall.value);
+//     clearInterval(fargeEndring);
+//     window.fargeEndring = setInterval(function() {
+
+//     }, tidsintervall);
+//     console.log("Endrar tidsintervall til: " + tidsintervall)
+// });
