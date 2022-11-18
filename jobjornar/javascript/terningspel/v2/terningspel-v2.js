218258a35f8f137bc513a5773e1d446109938cf6
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2022-11-18 11:03:10 +0100

    Terningspel

---
+++ jobjornar/javascript/terningspel/v2/terningspel-v2.js
@@ -0,0 +1,58 @@
+let treLike = false;    // settes til true når vi får to like
+let antallForsok = 0;  // lar oss telle antall forsøk
+const utskrift = document.querySelector("#utskrift");
+const resultat = document.querySelector("#resultat");
+
+// Så lenge vi ikke har fått to like
+while(!treLike) {  
+    // Øker antall forsøk med 1
+    antallForsok++;
+
+    // triller to terninger
+    let terning1 = terning();
+    let terning2 = terning();
+    let terning3 = terning();
+
+    console.log("Fikk " + terning1 + ", " + terning2 + ", " + terning3);
+    utskrift.innerHTML += "<li>Fikk " + terning1 + ", " + terning2 + ", " + terning3 + "</li>";
+
+    // Undersøker om de to terningene er like
+    if (terning1 === terning2 && terning2 === terning3) {
+        treLike = true;
+        console.log("Fikk tre like på " + antallForsok + " forsøk");
+        resultat.innerText = "Fikk tre like på " + antallForsok + " forsøk";
+    }
+}
+
+function terning() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+document.addEventListener("keydown", function(e) {
+    let tast = e.key;
+    if(tast==="k") {
+        
+    }
+});
+
+function kastTerning() {
+    antallForsok++;
+
+    // triller to terninger
+    let terning1 = terning();
+    let terning2 = terning();
+    let terning3 = terning();
+
+    console.log("Fikk " + terning1 + ", " + terning2 + ", " + terning3);
+    utskrift.innerHTML += "<li>Fikk " + terning1 + ", " + terning2 + ", " + terning3 + "</li>";
+
+    // Undersøker om de to terningene er like
+    if (terning1 === terning2 && terning2 === terning3) {
+        console.log("Fikk tre like på " + antallForsok + " forsøk");
+        resultat.innerText = "Fikk tre like på " + antallForsok + " forsøk";
+    }
+    else {
+        console.log("Ingen like på forsøk nr. " + antallForsok);
+        utskrift.innerHTML += "<li>Ingen like på forsøk nr. " + antallForsok + "</li>";
+    }
+}
