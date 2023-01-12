b0dc4bc76dd28f28b3ff5bb12d98212212fe0e1f
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-01-12 12:36:41 +0100

    Intro til moduler

---
+++ jobjornar/javascript/introduksjon-serie/intro-moduler/main.js
@@ -0,0 +1,7 @@
+let funksjoner = require("./module.js");
+
+let navnLengde = funksjoner.printTotalLength("Jo", "Bjørnar");
+console.log(navnLengde);
+
+let lengde = funksjoner.printTotalLength("bla","klo");
+console.log(lengde);
---
+++ jobjornar/javascript/introduksjon-serie/intro-moduler/module.js
@@ -0,0 +1,8 @@
+// 
+function printTotalLength(string1, string2) {
+    length = string1.length + string2.length;
+    //console.log(length);
+    return length;
+}
+
+exports.printTotalLength = printTotalLength;
