5f09ed638c52276e3faaf3be84ccc1343f17fc73
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-01-17 15:45:11 +0100

    Update randomuser.js

--- jobjornar/api/randomUserMe/randomuser.js
+++ jobjornar/api/randomUserMe/randomuser.js
@@ -12,13 +12,13 @@ function behandleSvar(svar) {
     console.log("Bilde: " + svar.results[0].picture.large);
 
     // HTML-visning
-    let navn = document.createElement("p");
-    navn.innerText = "Navn: " + svar.results[0].name.first;
+    let navn = document.createElement("p"); // Opprettar ein paragraf, men den er per no ikkje plassert
+    navn.innerText = "Navn: " + svar.results[0].name.first; // Endrar innhaldet i paragrafen
     let epost = document.createElement("p");
     epost.innerText = "Epost: " + svar.results[0].email;
     let bilde = document.createElement("img");
     bilde.src = svar.results[0].picture.large;
-    document.querySelector("#utskrift").appendChild(navn);
+    document.querySelector("#utskrift").appendChild(navn); // Plasserer paragrafen (navn) i HTML-en (DOM)
     document.querySelector("#utskrift").appendChild(epost);
     document.querySelector("#utskrift").appendChild(bilde);
 }
