1aa95e67a56b8eb5d311ba7a62331b6644776f06
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-03-16 15:33:42 +0100

    Update api.js

--- temp/metereologisk-test/api.js
+++ temp/metereologisk-test/api.js
@@ -5,8 +5,17 @@ fetch('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60.397076&
     .catch(err => console.error(err));
 
     function behandleSvar(svar) {
-        // Testings
+        // Testing
         console.log(svar);
-        console.log("vind" + svar.properties);
-        console.log("vind" + svar.properties.timeseries[0].data.instant.details.wind_speed);
+        console.log("Tidspunkt: " + svar.properties.timeseries[0].time);
+        console.log("Vind: " + svar.properties.timeseries[0].data.instant.details.wind_speed);
+        console.log("Temperatur: " + svar.properties.timeseries[0].data.instant.details.air_temperature);
+
+        // Henter ut for alle tidspunkta den perioden blei returnert i JSON-fila
+        for (let i = 0; i < svar.properties.timeseries.length; i++) {
+            const element = svar.properties.timeseries[i];
+            console.log("Tidspunkt: " + element.time);
+            console.log("Vind: " + element.data.instant.details.wind_speed);
+            console.log("Temperatur: " + element.data.instant.details.air_temperature);
+        }
 }
